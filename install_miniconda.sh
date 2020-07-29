7af714a8c6b3bb6654a2448d8e622285df8e9b3a
JTT94 <jtthornton1994@gmail.com>
2020-07-29 19:20:16 +0100

    rename

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,6 +1,8 @@
 conda_dir="$1"
 
+echo "Check for miniconda"
 if [ ! -d "miniconda3" ]; then 
+	echo "Installing miniconda"
 	curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o "$conda_dir/miniconda.sh"
 	sh -x miniconda.sh -b -p "$conda_dir/miniconda3"
 	rm -rf $conda_dir/miniconda.sh
--- setup_mflow_env.sh
+++ setup_mflow_env.sh
@@ -4,7 +4,7 @@ sh -x ./install_miniconda.sh $conda_dir
 
 $conda_dir/miniconda3/bin/conda create -n mlflow -y
 source $conda_dir/miniconda3/bin/activate mlflow
-conda install pip -y
+$conda_dir/miniconda3/bin/conda install pip -y
 pip install mlflow
 
 
