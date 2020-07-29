a40ab0d2e946a675d48785d885988cbe7a034460
JTT94 <jtthornton1994@gmail.com>
2020-07-29 19:34:34 +0100

    update mlflow setup with miniconda

--- setup_mlflow_env.sh
+++ setup_mlflow_env.sh
@@ -1,6 +1,7 @@
 conda_dir="$1"
+file_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sh -x ./install_miniconda.sh $conda_dir
+sh -x $file_dir/install_miniconda.sh $conda_dir
 
 $conda_dir/miniconda3/bin/conda create -n mlflow -y
 source $conda_dir/miniconda3/bin/activate mlflow
